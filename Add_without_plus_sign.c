4850ef19ab086cc21e05008d90bbad3f87c9b8ba
Oleksandra Baga <oleksart@gmail.com>
2016-07-27 15:47:18 +0200

    Update Add_without_plus_sign.c

--- Add_without_plus_sign.c
+++ Add_without_plus_sign.c
@@ -4,6 +4,7 @@ Realisation of Adder (Electronics) to calculate a sum of two integers under cond
 Using AND bit operand for generating Carry-out signal. XOR bit-operand for generating a simple sum. 
 */
 
+// Carry-Look-Ahead
 int getSum(int a, int b)
 {
 	int carry;
@@ -12,19 +13,10 @@ int getSum(int a, int b)
 	{
 		int c = a & b;
 		b = a ^ b;
-		carry = c;
-		printf("%i\n", b);
 		a = c << 1;
 
 	} while (a != 0);
 
 	return b;
 
-	if (carry) // If Carry-Out is 1
-	{
-		b = b | 0x0000;
-		b = b & 0x8000;
-	}
-
-	return b;
 }
