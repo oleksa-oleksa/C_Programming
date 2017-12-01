f8faab2b697f2f3702b7e64568472e54070de83a
Oleksandra Baga <oleksandra.baga@gmail.com>
2017-12-01 16:42:32 +0100

    Real Time signal works on Vagrant

--- 14_Fourth_Semester_Real_Time_Programming/03_Three_States_Process_Signals_Realtime_Signals/signal_VS_realtime_signal.c
+++ 14_Fourth_Semester_Real_Time_Programming/03_Three_States_Process_Signals_Realtime_Signals/signal_VS_realtime_signal.c
@@ -11,22 +11,20 @@ PID and UID of sending process is written to si_pid and si_uid fields of siginfo
 #include <stdlib.h>
 
 void handler(int signum) {
-    if(signum == SIGUSR1) {
+    if (signum == SIGUSR1) {
         printf("SIGUSR1 intercepted.\n");
     }
-    signal(SIGUSR1, SIG_DFL);
 }
 
-void handler_realTime(int signum, siginfo_t *si, void *data){
+void handler_realTime(int signum, siginfo_t *si, void *data) {
     if (signum == SIGRTMIN + 5) {
         printf("RealTime Signal %d intercepted. Data: %p\n", si->si_value.sival_int, data);
-        signal(SIGRTMIN + 5, SIG_DFL);
-
     }
 }
 
 int main() {
     // Real Time Signal Settings
+
     /* If act is non-NULL, the new action for signal signum is installed
        from act.  If oldact is non-NULL, the previous action is saved in oldact.*/
     struct sigaction act;
@@ -37,43 +35,29 @@ int main() {
     /* The signal handler takes three arguments, not one.  In this case, sa_sigaction should be set instead of sa_handler.*/
     act.sa_flags = SA_SIGINFO;
     sigaction(SIGRTMIN + 5, &act, NULL); // Task is to be presented In UBUNTU
-					 // No Real Time Signal Support on MacOS
+    // No Real Time Signal Support on MacOS
 
     union sigval v;
     v.sival_int = 10;
-    
+
     // Regular Signal Settings
-    sigset_t set, signalset;
+    sigset_t signalset;
     signal(SIGUSR1, handler);
+    sigemptyset(&signalset);
     sigaddset(&signalset, SIGUSR1);
     sigaddset(&signalset, SIGRTMIN + 5);
-    while(1) {
-        /* The sigprocmask() function examines and/or changes the current signal mask (those signals that are blocked from delivery).
-         * Signals are blocked if they are members of the current signal mask set.*/
-        sigprocmask(SIG_BLOCK, &signalset, NULL);
-        puts("Signals blocking: activated\n");
-        printf("Please sent SIGUSR1 to a PID: %u or wait for RT Signal message.\n", getpid());
 
-        sleep(10);
-        // SENDING REAL TIME SIGNAL
-        sigqueue(getpid(), SIGRTMIN + 5, v);
-        printf("RT signal was sent\n");
-        sleep(10);
-
-        /* sigpending() returns the set of signals that are pending
-         * for delivery to the calling thread (i.e., the signals which have been raised while blocked).*/
-        sigpending(&set);
-
-        if (sigismember(&set, SIGUSR1)){
-            puts("SIGUSR1 is pending");
-        }
-        if (sigismember(&set, SIGRTMIN + 5)){
-            puts("RT Signal is pending");
-        }
-        sigprocmask(SIG_UNBLOCK, &signalset, NULL);
-        puts("Signals blocking: disabled\n");
-        sleep(3);
-    }
-
+    /* The sigprocmask() function examines and/or changes the current signal mask (those signals that are blocked from delivery).
+     * Signals are blocked if they are members of the current signal mask set.*/
+    sigprocmask(SIG_BLOCK, &signalset, NULL);
+    puts("Signals blocking: activated\n");
+    kill(getpid(), SIGUSR1);
+    printf("SIGUSR1 was sent\n");
+    sigqueue(getpid(), SIGRTMIN + 5, v);
+    printf("RT signal was sent\n");
+    sleep(3);
+    puts("Signals blocking: disabled\n");
+    sigprocmask(SIG_UNBLOCK, &signalset, NULL);
+    sleep(3);
     return EXIT_SUCCESS;
 }
--- 14_Fourth_Semester_Real_Time_Programming/03_Three_States_Process_Signals_Realtime_Signals/signalset_implementation_swp.c
+++ 14_Fourth_Semester_Real_Time_Programming/03_Three_States_Process_Signals_Realtime_Signals/signalset_implementation_swp.c
@@ -26,176 +26,128 @@ typedef struct sigNode_t {
     struct sigNode_t *right;
 } sigNode;
 
-typedef struct {
-    sigNode *root;
+typedef struct tree_t {
+    sigNode *node;
 } tree;
 
-int readSigno();
-int my_sigdelset(int signo, tree *t);
-int my_sigdelnode(int signo, sigNode *root);
-sigNode *getMinNode(sigNode *leaf);
-int my_sigismember(int signo, tree *t);
-int my_sigisnode(int signo, sigNode *root);
-void my_sigfillset(tree *root);
-void my_sigemptyset(tree *t);
-void my_sigaddset(int signo, tree *t);
-void add_node(sigNode *leaf, sigNode *root);
-void print_tree(tree *t);
+tree *root = NULL;
 
-
-        void print_node(sigNode *root) {
-    if(root == NULL){
-        return;
-    }
-    if(root->left){
-        print_node(root->left);
-    }
+void print(tree *root) {
+  puts("A");
+  if(root == NULL) return;
+    if(root->left) print(root->left);
     printf("%d\n", root->signo);
-    if(root->right){
-        print_node(root->right);
-    }
-}
-
-void print_tree(tree *t) {
-    print_node(t->root);
-}
-
-
-void add_node(sigNode *leaf, sigNode *root) {
-
-    while (root != NULL) {
-        if (leaf->signo > root->signo)
-            if (root->right) {
-                root = root->right;
-            } else {
-                root->right = leaf;
-                return;
-            }
-        else {
-            if (root->left) {
-                root = root->left;
-            } else {
-                root->left = leaf;
-                return;
-            }
-        }
-    }
+    if(root->right) print(root->right);
 }
 
 
 /* Creates a new tree if necessary
  */
-void my_sigaddset(int signo, tree *t) {
-    if(t == NULL) {
-        return;
-    }
-    sigNode *n = malloc(sizeof(sigNode));
-    n->left=NULL;
-    n->right=NULL;
-    n->signo = signo;
-
-    if(t->root == NULL) {
-        t->root = n;
-        puts("New root\n");
-    }
-    else add_node(n, t->root);
-}
-
-void my_sigemptyset(tree *t)
-{
-    if(t != NULL )
+void my_sigaddset(int new_signo, tree *root) {
+    if(root == NULL)
     {
-        for(int i = 31; i <= 0; i--){
-            my_sigdelnode(i, root);
-        }
+        root = malloc(sizeof(sigNode));
+        root->node->signo = new_signo;
+        /* initialize the children to null */
+        root->node->left = NULL;
+        root->node->right = NULL;
+    }
+    else if(new_signo < root->node->signo)
+    {
+        my_sigaddset(new_signo, root->node->left);
+    }
+    else if(new_signo > root->node->signo)
+    {
+        my_sigaddset(new_signo, root->node->right);
     }
 }
 
-void my_sigfillset(tree *root) {
+void my_sigemptyset(tree *root)
+{
+    if(root != NULL )
+    {
+        my_sigemptyset(root->node->left);
+        my_sigemptyset(root->node->right);
+        free(root);
+    }
+}
+
+sigNode *my_sigfillset(tree *root){
     for(int i = 1; i <= 31; i++){
         my_sigaddset(i, root);
     }
+    return root;
 }
 
-int my_sigisnode(int signo, sigNode *root)
+int my_sigismember(int new_signo, tree *root)
 {
-    while (root != NULL){
-        if (signo < root->signo){
-            root = root->left;
-        }
-        if (signo > root->signo){
-            root = root->right;
-        }
-        if (signo == root->signo){
+    if(root != NULL)
+    {
+        if(new_signo == root->node->signo)
+        {
             return 1;
         }
+        else if(new_signo < root->node->signo)
+        {
+            return my_sigismember(new_signo, root->node->left);
+        }
         else
-            return 0;
-
+        {
+            return my_sigismember(new_signo, root->node->right);
+        }
     }
-    return 0;
+    else return 0;
 }
 
-int my_sigismember(int signo, tree *t) {
-    if (t != NULL && t->root != NULL)
-    {
-        return my_sigisnode(signo, t->root);
-    }
-    return 0;
-}
-
-sigNode *getMinNode(sigNode *leaf){
-    if (leaf == NULL){
+sigNode *getMinNode(tree *root){
+    if (root == NULL){
         return NULL;
     }
     // if left tree exists - the smallest node is there
-    if (leaf->left != NULL){
-        return getMinNode(leaf->left);
+    if (root->node->left != NULL){
+        return getMinNode(root->node->left);
     }
     else {
-        return leaf;
+        return root;
     }
 
 }
 
-int my_sigdelnode(int signo, sigNode *root) {
-    if (root == NULL){
+int my_sigdelset(int del_signo, tree *root){
+    if (root == NULL) {
         return 0;
     }
     // signai is not supported
-    if (root->signo <= 0 || root->signo > 31) {
+    if (root->node->signo <= 0 || root->node->signo > 31) {
         return -1;
     }
-    while (root != NULL) {
-        if (signo < root->signo) {  // data is in the left sub tree.
-            root = root->left;
-        }
-        if (signo > root->signo) {  // data is in the right sub tree.
-            root = root->right;
-        }
-        if (signo == root->signo) {
+    if (leaf != NULL){
+        if (del_signo < root->node->signo) {  // data is in the left sub tree.
+            my_sigdelset(del_signo, root->node->left);
+        } else if (del_signo > root->node->signo) { // data is in the right sub tree.
+            my_sigdelset(del_signo, root->node->right );
+        } else {
             // case 1: no children
-            if (root->left == NULL && root->right == NULL) {
-                free(root);
+            if (root->node->left == NULL && root->node->right == NULL) {
+                free(root); // wipe out the memory, in C, use free function
             }
-                // case 2: one child (right)
-            else if (root->left == NULL && root->right != NULL) {
+            // case 2: one child (right)
+            else if (root->node->left == NULL && root->node->right != NULL) {
                 sigNode *temp = root; // save current node as a backup
-                root = root->right;
+                root = root->node->right;
                 free(temp);
             }
-                // case 3: one child (left)
-            else if (root->right == NULL && root->left != NULL) {
+            // case 3: one child (left)
+            else if (root->node->right == NULL && root->node->left != NULL) {
                 sigNode *temp = root; // save current node as a backup
-                root = root->left;
+                root = root->node->left;
                 free(temp);
             }
-                // case 4: two children
+            // case 4: two children
             else {
-                sigNode *minNode = getMinNode(
-                        root->right); // find minimal value of right sub tree - new root for a sub-tree
+                sigNode *minNode = getMinNode(root->node->right); // find minimal value of right sub tree - new root for a sub-tree
                 sigNode *temp = root; // node-to-be-deleted
-                root->signo = minNode->signo; // duplicate the node
+                root->node->signo = minNode->signo; // duplicate the node
                 free(minNode);
                 free(temp);
                 return 0;
@@ -203,15 +155,6 @@ int my_sigdelnode(int signo, sigNode *root) {
         }
     }
     return 0;
-}
-
-int my_sigdelset(int signo, tree *t){
-    if (t == NULL) {
-        return 0;
-    }
-    else {
-        return my_sigdelnode(signo, t->root);
-    }
 }
 
 int readSigno(){
@@ -233,8 +176,6 @@ int readSigno(){
 int main() {
     int signo, isInSet;
     clearScreen();
-    tree t;
-    t.root = NULL;
     int choice;
     char *Menu[] = {"Add all signals to a set",
                     "Add a signal to a set",
@@ -246,33 +187,34 @@ int main() {
     do {
         clearScreen();
         choice = getMenu("Signal Set Management", Menu, 6);
+
         switch (choice) {
             case 1:
-                my_sigfillset(&t);
+                root = my_sigfillset(root);
                 printf("Signals 1 - 31 were added in a set.\n");
                 waitForEnter();
                 break;
             case 2:
                 signo = readSigno();
-                my_sigaddset(signo, &t);
+                my_sigaddset(signo, root);
                 printf("Signal %d was added in a set.\n", signo);
-                print_tree(&t);
+                print(root);
                 waitForEnter();
                 break;
             case 3:
-                //my_sigemptyset(t->root);
+                my_sigemptyset(root);
                 printf("Signal set is empty.\n");
                 waitForEnter();
                 break;
             case 4:
                 signo = readSigno();
-                my_sigdelset(signo, &t);
+                my_sigdelset(signo, root);
                 printf("Signal %d was deleted from a set.\n", signo);
                 waitForEnter();
                 break;
             case 5:
                 signo = readSigno();
-                isInSet = my_sigismember(signo, &t);
+                isInSet = my_sigismember(signo, root);
                 if (isInSet){
                     printf("Signal %d is in a set.\n", signo);
                     waitForEnter();
