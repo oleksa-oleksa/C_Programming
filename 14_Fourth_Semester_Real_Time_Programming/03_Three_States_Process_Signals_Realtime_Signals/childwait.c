2785647fc822de4ba70a55e7e070a48f03d6cfdf
Oleksandra Baga <oleksandra.baga@gmail.com>
2017-12-07 00:44:22 +0100

    childwait fork by exit

--- 14_Fourth_Semester_Real_Time_Programming/03_Three_States_Process_Signals_Realtime_Signals/childwait.c
+++ 14_Fourth_Semester_Real_Time_Programming/03_Three_States_Process_Signals_Realtime_Signals/childwait.c
@@ -9,16 +9,19 @@
 
 #define CHILD 0
 #define FAILURE -1
+int n = 0;
 
-void signal_childHandler(int signo){
-    if (signo == 20){
+void signal_childHandler() {
+    pid_t pid = 0;
+    n -= 1; // decrement if child dies
+    pid = wait(NULL);
+    printf("Child %d: terminated (n = %d)\n", pid, n);
 
-    }
 }
 
-int main (int args, char *argv[]) {
+int main(int args, char *argv[]) {
 
-    if (args != 2){
+    if (args != 2) {
         printf("Usage: childwait <amount of children>\n");
         return EXIT_SUCCESS;
     }
@@ -26,19 +29,25 @@ int main (int args, char *argv[]) {
     signal(SIGCHLD, signal_childHandler);
 
     int k = atoi(argv[1]);
+    pid_t pids[k];
 
-    int p = fork();
-
-    if (p == FAILURE) {
-        perror("fork() failed\n");
+    if (k > 0) {
+        for (int i = 0; i < k; i++) {
+            pids[i] = fork();
+            if (pids[i] == FAILURE) {
+                perror("fork() failed\n");
+            } else if (pids[i] == CHILD) {
+                printf("Child %d: started (n = %d)\n", getpid(), n);
+                sleep(1);
+                EXIT_SUCCESS;
+            }
+            n += 1;
+        }
     }
-    if (p > CHILD) {
-        // Grandpa
-        printf("Grandpa: %d\n", getpid());
-        sleep(100);
-        waitpid(p, NULL, 0);
-    }
-
-
+    do {
+        printf("Parent %d: sleep(2)\n", getpid());
+        sleep(2);
+    } while (!n);
+    EXIT_SUCCESS;
 }
 
